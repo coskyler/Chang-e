4731bf26a561d9c85ff914196031da3c8952eb69
Lou Vaughn <ljvaughn0841>
2025-10-26 02:23:19 -0400

    Stock Axis Colors

--- frontend/src/components/quote/StockChart.js
+++ frontend/src/components/quote/StockChart.js
@@ -51,39 +51,36 @@ export default function StockChart({ width = 800, ratio = 3, type = "svg" }) {
       data={chartData} xScale={xScale} xAccessor={xAccessor} displayXAccessor={displayXAccessor} xExtents={xExtents}
     >
       <Chart id={1} height={400} yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]} padding={{ top: 10, bottom: 20 }}>
-        <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
-        <YAxis axisAt="right" orient="right" ticks={5} />
+        <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} tickStrokeStyle="#FFFFFF" strokeStyle="#FFFFFF" tickLabelFill="#FFFFFF"/>
+        <YAxis axisAt="right" orient="right" ticks={5} tickStrokeStyle="#FFFFFF" strokeStyle="#FFFFFF" tickLabelFill="#FFFFFF"/>
         <MouseCoordinateY at="right" orient="right" displayFormat={format(".2f")} />
-        <CandlestickSeries />
-        <LineSeries yAccessor={ema26.accessor()} stroke={ema26.stroke()} />
+        {/* <CandlestickSeries /> */}
         <LineSeries yAccessor={ema12.accessor()} stroke={ema12.stroke()} />
-        <CurrentCoordinate yAccessor={ema26.accessor()} fill={ema26.stroke()} />
         <CurrentCoordinate yAccessor={ema12.accessor()} fill={ema12.stroke()} />
         <EdgeIndicator itemType="last" orient="right" edgeAt="right" yAccessor={d => d.close} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"} />
-        <OHLCTooltip origin={[-40, 0]} />
+        <OHLCTooltip origin={[-40, 0]} textFill="#FFFFFF" labelFill="#AAAAAA" />
         <MovingAverageTooltip
           origin={[-38, 15]}
           options={[
-            { yAccessor: ema26.accessor(), type: ema26.type(), stroke: ema26.stroke(), windowSize: ema26.options().windowSize },
             { yAccessor: ema12.accessor(), type: ema12.type(), stroke: ema12.stroke(), windowSize: ema12.options().windowSize },
           ]}
         />
       </Chart>
 
-      <Chart id={2} height={150} yExtents={[d => d.volume, smaVolume50.accessor()]} origin={(w, h) => [0, h - 300]}>
+      {/* <Chart id={2} height={150} yExtents={[d => d.volume, smaVolume50.accessor()]} origin={(w, h) => [0, h - 300]}>
         <YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} />
         <MouseCoordinateY at="left" orient="left" displayFormat={format(".4s")} />
         <BarSeries yAccessor={d => d.volume} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"} />
         <AreaSeries yAccessor={smaVolume50.accessor()} stroke={smaVolume50.stroke()} fill={smaVolume50.fill()} />
-      </Chart>
-
+      </Chart> */}
+{/* 
       <Chart id={3} height={150} yExtents={macdCalculator.accessor()} origin={(w, h) => [0, h - 150]} padding={{ top: 10, bottom: 10 }}>
         <XAxis axisAt="bottom" orient="bottom" />
         <YAxis axisAt="right" orient="right" ticks={2} />
         <MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat("%Y-%m-%d")} />
         <MouseCoordinateY at="right" orient="right" displayFormat={format(".2f")} />
         <MACDSeries yAccessor={d => d.macd} {...macdAppearance} />
-      </Chart>
+      </Chart> */}
 
       <CrossHairCursor />
     </ChartCanvas>
