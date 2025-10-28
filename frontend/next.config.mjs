e5da0fe5199434672254fb23f06334bcda7ce2b9
Skyler Quinby <skylerpquinby@gmail.com>
2025-10-28 05:20:26 -0400

    Proxy api calls

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/stock/:path*",
+        destination: "https://api.changefinance.coskyler.com/stock/:path*",
+      },
+      {
+        source: "/user/:path*",
+        destination: "https://api.changefinance.coskyler.com/user/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
